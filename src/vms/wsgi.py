45c16f7d4ef336bfb21b3ca3a02ca0ad1019e925
Mihai Man <mihai.man@3pillarglobal.com>
2017-05-19 17:17:25 +0300

    updated settings

--- src/vms/wsgi.py
+++ src/vms/wsgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vms.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vms.dev_settings")
+os.environ.setdefault("SECRET_KEY",
+                      "gy%^a=g_#$%eon#pab)@m9o=7=qwzjd--@9d3t^de83b#qphg#")
+os.environ.setdefault("DB_PWD", "TIFFsubIT")
 
 application = get_wsgi_application()
