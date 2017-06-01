ce686f256a2712b100935a53ca0dca1c017e087c
Mihai Man <mitaman89@yahoo.com>
2017-06-01 11:16:39 +0300

    created mock volunteer schedule endpoint

--- src/vms/volunteers/urls.py
+++ src/vms/volunteers/urls.py
@@ -5,4 +5,6 @@ from vms.volunteers.views import base
 urlpatterns = [
     url(r'^$', base.index, name='index'),
     url(r'^volunteers$', base.VolunteersView.as_view(), name='volunteers_list'),
+    # url(r'^schedule$', base.ScheduleView.as_view(), name='schedule'),
+    url(r'^schedule$', base.MockSchedule.as_view(), name='schedule'),
 ]
--- src/vms/volunteers/views/base.py
+++ src/vms/volunteers/views/base.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
-from django.views.generic import ListView
+from django.views.generic import ListView, TemplateView
 
-from vms.entities.models import Volunteer
+from vms.entities.models import Volunteer, VolunteerSchedule
 
 
 def index(request):
@@ -15,3 +15,16 @@ class VolunteersView(ListView):
     def get_queryset(self):
         qs = super().get_queryset()
         return qs.order_by('name')
+
+
+class ScheduleView(ListView):
+    model = VolunteerSchedule
+    template_name = 'schedule.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.order_by('projection__date')
+
+
+class MockSchedule(TemplateView):
+    template_name = 'schedule.html'
