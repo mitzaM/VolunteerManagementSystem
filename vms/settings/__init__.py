099c1f993cdbe4c323f1d174050b56e90435d684
Mihai Man <mitaman89@yahoo.com>
2022-01-09 15:29:49 +0200

    Fix settings

--- vms/settings/__init__.py
+++ vms/settings/__init__.py
@@ -1,8 +1,6 @@
-from dotenv import load_dotenv
+import os
 
 from vms.settings.base import *
-
-load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 environment = os.getenv('ENVIRONMENT')
 if environment == 'production':
